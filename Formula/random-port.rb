3536d5bb4d913bc551ce3ef4216dba172b52a917
Oscar <48883102+OZCAP@users.noreply.github.com>
2023-11-03 18:37:20 +0200

    0.9.1

--- Formula/random-port.rb
+++ Formula/random-port.rb
@@ -1,9 +1,9 @@
 class RandomPort < Formula
     desc "Simple CLI tool for generating a random and safe port number to use for local development"
     homepage "https://github.com/OZCAP/random-port"
-    url "https://github.com/OZCAP/random-port/releases/download/0.9/random-port-mac.tar.gz"
-    sha256 "5524e6dc29afe9401842a69dfe4dd062d70f27075485e9d5b5c981e85e9397c2"
-    version "0.9"
+    url "https://github.com/OZCAP/random-port/releases/download/0.9.1/random-port-mac.tar.gz"
+    sha256 "b172d37fe8f87a87947794c5fae5c842d47fdd983b15e9ee9bc5971872c21f49"
+    version "0.9.1"
   
     def install
       bin.install "random-port"
